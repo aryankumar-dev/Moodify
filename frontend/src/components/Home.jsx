b96edb90d495339f4a62fd1245f82695a29c5e22
aryanaryan <103895665+aryankumar-dev@users.noreply.github.com>
2025-06-23 17:24:53 +0530

    Added Home Page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,25 @@
 import Nav from "./Nav.jsx";
-
+import "./Home.css"
 function Home() {
-  return (
-    <>
-      <Nav />
-      <div>Home</div>
-    </>
-  );
+    return (
+        <>
+            <Nav />
+            <div className="choosemood-box col-8 mx-auto mt-5 text-white">
+            <div>
+
+                <p className='display-5 fw-bolder' >Choose Your Mood</p>
+                <p>Select your current mood and we'll create a playlistjust for you. </p>
+                <button className="boxbtn text-white">Happy</button>
+            </div> </div>
+
+            <div className="moods text-white col-4 mx-auto mt-5">
+                <button>Sad</button>
+                <button>Energetic</button>
+                <button>Chill</button>
+                <button>Focus</button>
+            </div>
+        </>
+    );
 }
 
 export default Home;
--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -6,13 +6,13 @@ function Nav() {
     return (
 
         <nav className="navbar navbar-light bg-light justify-content-between">
-            <a className="navbar-brand">Navbar</a>
+            <a className="navbar-brand">Moodify</a>
             <div>
-                <a className="navbar-brand">Navbar</a>
-                <a className="navbar-brand">Navbar</a>
-                <a className="navbar-brand">Navbar</a>
-                <a className="navbar-brand">Navbar</a>
-                <a className="navbar-brand">Navbar</a>
+                <a className="navbar-brand">Home</a>
+                <a className="navbar-brand">Explore</a>
+                <a className="navbar-brand">Your Libarary</a>
+                <a className="navbar-brand">@</a>
+                <a className="navbar-brand">Logo</a>
             </div>
 
         </nav>
